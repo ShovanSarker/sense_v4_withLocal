66dd26e9c2142bb0f0426ef3cccdc0257a8b4905
Shovan Sarker <exorcist.shovan@gmail.com>
2016-01-13 16:00:09 +0600

    VXML updated big time!!!

--- voice_records/views.py
+++ voice_records/views.py
@@ -7,6 +7,7 @@ from .models import VoiceRecord, VoiceReg
 from subscriber.models import Consumer
 from transcriber_management.models import FailedTranscription, Transcriber
 from transaction.models import Transaction
+import os, shutil
 # Create your views here.
 
 
@@ -24,6 +25,8 @@ def save_record_voice(request):
         tracking_id = get_data['tid']
     else:
         tracking_id = '0000'
+    tempname=''
+
     if get_data['level'] == '1':
         if get_data['part'] == '1':
             tracking_id = phone[-4:]
@@ -31,12 +34,19 @@ def save_record_voice(request):
             now = ''.join(x for x in str(datetime.datetime.now()) if x.isdigit())
             tracking_id += now[2:14]
             caller_object = Consumer.objects.get(phone__endswith=phone)
+            # save_record = None
             save_record = VoiceRecord(caller=caller_object,
                                       level1_voice_part1=file_data['rec1'],
                                       call_tracking_id=tracking_id,
                                       purpose=get_data['purpose'],
                                       level=get_data['level'])
             save_record.save()
+            tempname = str(save_record.level1_voice_part1)
+            filename = '/home/exor/static/media/' + tempname
+            filenamenew = '/home/exor/static/media/' + tempname[:-3] + 'wav'
+            shutil.copy2(filename, filenamenew)
+            print(filename)
+            print(filenamenew)
             if get_data['purpose'] == 'Sell':
                 sell = True
             else:
@@ -49,6 +59,10 @@ def save_record_voice(request):
             save_record = VoiceRecord.objects.get(call_tracking_id__exact=tracking_id)
             save_record.level1_voice_part2 = file_data['rec2']
             save_record.save()
+            tempname = str(save_record.level1_voice_part2)
+            filename = '/home/exor/static/media/' + tempname
+            filenamenew = '/home/exor/static/media/' + tempname[:-3] + 'wav'
+            shutil.copy2(filename, filenamenew)
             if get_data['purpose'] == 'Sell':
                 sell = True
             else:
@@ -60,6 +74,10 @@ def save_record_voice(request):
             save_record = VoiceRecord.objects.get(call_tracking_id__exact=tracking_id)
             save_record.level1_voice_part3 = file_data['rec3']
             save_record.save()
+            tempname = str(save_record.level1_voice_part3)
+            filename = '/home/exor/static/media/' + tempname
+            filenamenew = '/home/exor/static/media/' + tempname[:-3] + 'wav'
+            shutil.copy2(filename, filenamenew)
             return render(request, 'IVR/end.xml', content_type='application/xml')
     elif get_data['level'] == '2':
         if get_data['part'] == '1':
@@ -74,6 +92,10 @@ def save_record_voice(request):
                                       purpose=get_data['purpose'],
                                       level=get_data['level'])
             save_record.save()
+            tempname = str(save_record.level1_voice_part1)
+            filename = '/home/exor/static/media/' + tempname
+            filenamenew = '/home/exor/static/media/' + tempname[:-3] + 'wav'
+            shutil.copy2(filename, filenamenew)
             if get_data['purpose'] == 'Sell':
                 sell = True
             else:
@@ -86,6 +108,10 @@ def save_record_voice(request):
             save_record = VoiceRecord.objects.get(call_tracking_id__exact=tracking_id)
             save_record.level1_voice_part2 = file_data['rec42']
             save_record.save()
+            tempname = str(save_record.level1_voice_part2)
+            filename = '/home/exor/static/media/' + tempname
+            filenamenew = '/home/exor/static/media/' + tempname[:-3] + 'wav'
+            shutil.copy2(filename, filenamenew)
             if get_data['purpose'] == 'Sell':
                 sell = True
             else:
@@ -97,6 +123,10 @@ def save_record_voice(request):
             save_record = VoiceRecord.objects.get(call_tracking_id__exact=tracking_id)
             save_record.level1_voice_part3 = file_data['rec43']
             save_record.save()
+            tempname = str(save_record.level1_voice_part3)
+            filename = '/home/exor/static/media/' + tempname
+            filenamenew = '/home/exor/static/media/' + tempname[:-3] + 'wav'
+            shutil.copy2(filename, filenamenew)
             return render(request, 'IVR/end.xml', content_type='application/xml')
     elif get_data['level'] == '3':
         tracking_id = phone[-4:]
@@ -109,6 +139,10 @@ def save_record_voice(request):
                                   purpose=get_data['purpose'],
                                   level=get_data['level'])
         save_record.save()
+        tempname = str(save_record.level3)
+        filename = '/home/exor/static/media/' + tempname
+        filenamenew = '/home/exor/static/media/' + tempname[:-3] + 'wav'
+        shutil.copy2(filename, filenamenew)
         return render(request, 'IVR/end.xml', content_type='application/xml')
     else:
         return render(request, 'IVR/base.xml', content_type='application/xml')
@@ -118,23 +152,25 @@ def save_record_voice(request):
 def save_registration_voice(request):
     file_data = request.FILES
     get_data = request.GET
+    print(get_data)
+    print(file_data)
     if 'level' in get_data:
         if get_data['level'] == '1':
             phone = get_data['session.callerid']
             caller_type = get_data['caller_type']
-            print('upto here')
-            print(get_data)
+            # print('upto here')
+            # print(get_data)
             if 'session.calledid' in get_data:
                 calledNumber = get_data['session.calledid']
-                print(calledNumber[-4:])
+                # print(calledNumber[-4:])
                 if calledNumber[-4:] == '0010':
                     phone = '880' + phone[-10:]
-                    print('bangladesh')
+                    # print('bangladesh')
                 elif calledNumber[-4:] == '4436':
-                    print('singapore')
+                    # print('singapore')
                     phone = '65' + phone[-8:]
             tracking_id = phone[-4:]
-            print(phone)
+            # print(phone)
             now = ''.join(x for x in str(datetime.datetime.now()) if x.isdigit())
             tracking_id += now[2:14]
             new_voice_reg = VoiceReg(caller=phone,
@@ -153,6 +189,12 @@ def save_registration_voice(request):
             new_voice_reg = VoiceReg.objects.get(tracking_id=tracking_id)
             new_voice_reg.registration_voice_name = file_data['rec2']
             new_voice_reg.save()
+            # new_voice_regname = new_voice_reg.registration_voice_name
+            # print(new_voice_regname)
+            tempname = str(new_voice_reg.registration_voice_name)
+            filename = '/home/exor/static/media/' + tempname
+            filenamenew = '/home/exor/static/media/' + tempname[:-3] + 'wav'
+            shutil.copy2(filename, filenamenew)
             if new_voice_reg.caller_type == 'Shop':
                 shop = True
             else:
@@ -164,6 +206,10 @@ def save_registration_voice(request):
             new_voice_reg = VoiceReg.objects.get(tracking_id=tracking_id)
             new_voice_reg.registration_voice_address = file_data['rec3']
             new_voice_reg.save()
+            tempname = str(new_voice_reg.registration_voice_address)
+            filename = '/home/exor/static/media/' + tempname
+            filenamenew = '/home/exor/static/media/' + tempname[:-3] + 'wav'
+            shutil.copy2(filename, filenamenew)
             if new_voice_reg.caller_type == 'Shop':
                 return render(request, 'IVR/registration_5.xml', {'tracking_id': tracking_id},  content_type='application/xml')
             else:
@@ -173,12 +219,20 @@ def save_registration_voice(request):
             new_voice_reg = VoiceReg.objects.get(tracking_id=tracking_id)
             new_voice_reg.registration_voice_age = file_data['rec4']
             new_voice_reg.save()
+            tempname = str(new_voice_reg.registration_voice_age)
+            filename = '/home/exor/static/media/' + tempname
+            filenamenew = '/home/exor/static/media/' + tempname[:-3] + 'wav'
+            shutil.copy2(filename, filenamenew)
             return render(request, 'IVR/registration_5.xml', {'tracking_id': tracking_id},  content_type='application/xml')
         elif get_data['level'] == '5':
             tracking_id = get_data['tracking_id']
             new_voice_reg = VoiceReg.objects.get(tracking_id=tracking_id)
             new_voice_reg.registration_voice_intro = file_data['rec5']
             new_voice_reg.save()
+            tempname = str(new_voice_reg.registration_voice_intro)
+            filename = '/home/exor/static/media/' + tempname
+            filenamenew = '/home/exor/static/media/' + tempname[:-3] + 'wav'
+            shutil.copy2(filename, filenamenew)
             return render(request, 'IVR/end.xml', content_type='application/xml')
     else:
         return render(request, 'IVR/end.xml', content_type='application/xml')
@@ -197,8 +251,9 @@ def voice_record_list(request):
                                                                         call.level,
                                                                         call.caller.phone,
                                                                         str(call.DateAdded))
-        audio = '%s,%s,%s,%s,%s,' % (call.level1_voice_part1, call.level1_voice_part2,
-                                     call.level1_voice_part3, call.level2, call.level3)
+        audio = '%s,%s,%s,%s,%s,' % (str(call.level1_voice_part1)[:-3]+'wav', str(call.level1_voice_part2)[:-3]+'wav',
+                                     str(call.level1_voice_part3)[:-3]+'wav', str(call.level2)[:-3]+'wav',
+                                     str(call.level3)[:-3]+'wav')
         audio = audio.strip(',')
         output += '"audiofiles": "%s"},' % audio
         i += 1
@@ -213,8 +268,10 @@ def voice_record_list(request):
                                           '"call_timestamps": "%s",' % (call.pk,
                                                                         call.caller, call.caller_type,
                                                                         call.caller_type +' -> '+str(call.DateAdded))
-        audio = '%s,%s,%s,%s,' % (call.registration_voice_name, call.registration_voice_address,
-                                  call.registration_voice_age, call.registration_voice_intro)
+        audio = '%s,%s,%s,%s,' % (str(call.registration_voice_name)[:-3]+'wav',
+                                  str(call.registration_voice_address)[:-3]+'wav',
+                                  str(call.registration_voice_age)[:-3]+'wav',
+                                  str(call.registration_voice_intro)[:-3]+'wav')
         audio = audio.strip(',')
         output += '"audiofiles": "%s"},' % audio
         i += 1
@@ -267,8 +324,9 @@ def completed_voice_record_list(request):
                                                                         call.level,
                                                                         call.caller.phone,
                                                                         str(call.DateAdded))
-        audio = '%s,%s,%s,%s,%s,' % (call.level1_voice_part1, call.level1_voice_part2,
-                                     call.level1_voice_part3, call.level2, call.level3)
+        audio = '%s,%s,%s,%s,%s,' % (str(call.level1_voice_part1)[:-3]+'wav', str(call.level1_voice_part2)[:-3]+'wav',
+                                     str(call.level1_voice_part3)[:-3]+'wav', str(call.level2)[:-3]+'wav',
+                                     str(call.level3)[:-3]+'wav')
         audio = audio.strip(',')
         output += '"audiofiles": "%s"},' % audio
         i += 1
