e385a986caa16d41c44a154d58cc035b7bea247b
Shovan Sarker <exorcist.shovan@gmail.com>
2016-02-13 15:15:42 +0600

    SMS statement added !

--- template_manager/views.py
+++ template_manager/views.py
@@ -1165,15 +1165,8 @@ def add_subscriber_page(request):
             if total_sales > 0:
                 sales_statement = ' bikroy korechen mot: ' + str(total_sales) + ' takar. nogod peyechen : ' + \
                                   str(total_paid) + ' taka ebong baki royeche ' + str(total_due) + ' taka.'
-            # for buying
-            # if item_for_delete.type.type_name == 'Distributor':
-            #     all_srs = ACL.objects.filter(distUser=item_for_delete)
-
             buy_transactions = Transaction.objects.filter(buyer=item_for_delete, DateAdded__day=today_day,
                                                           DateAdded__month=today_month, DateAdded__year=today_year)
-            # if item_for_delete.type.type_name == 'Distributor':
-            # buy_transactions = Transaction.objects.filter(buyer=item_for_delete, DateAdded__day=today_day,
-            #                                               DateAdded__month=today_month, DateAdded__year=today_year)
             total_purchase = 0
             total_due = 0
             total_paid = 0
